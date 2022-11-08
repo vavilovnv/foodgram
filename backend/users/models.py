739c9edb6ba6c11a834418d2b323c6d053b47e43
nike <vavilovnv@gmail.com>
2022-11-08 12:12:29 +0300

    The first settings are done. Users app added.

---
+++ backend/users/models.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
+from django.db import models
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    objects = UserManager()
+
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        blank=False,
+        verbose_name='Имя пользователя',
+    )
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name='Имя',
+    )
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name='Фамилия',
+    )
+    email = models.EmailField(
+        max_length=254,
+        unique=True,
+        blank=False,
+        verbose_name='E-mail',
+    )
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    USERNAME_FIELD = 'email'

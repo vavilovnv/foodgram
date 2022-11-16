bed21605f3aac08f1a0ee6ed23014a0ae21c6ec3
nike <vavilovnv@gmail.com>
2022-11-16 17:23:24 +0300

    Deploy fix.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -3,7 +3,8 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-load_dotenv()
+env_path = Path('../infra') / '.env'
+load_dotenv(dotenv_path=env_path)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 

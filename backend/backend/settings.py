bf8bd144b20835f329997b4d17216e17e5d0a9f0
nike <vavilovnv@gmail.com>
2022-11-19 12:13:01 +0300

    Restored.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -127,6 +127,9 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 6
 }

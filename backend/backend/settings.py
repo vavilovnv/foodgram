1879640463d6a569614bf897c272faeb046de3e9
nike <vavilovnv@gmail.com>
2022-11-19 12:07:56 +0300

    Restored.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -128,10 +128,7 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 6,
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-    )
+    'PAGE_SIZE': 6
 }
 
 DJOSER = {
